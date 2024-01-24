0131565c330f3079f22c423a58ac17de64439574
yunaahn <86057836+yunaahn@users.noreply.github.com>
2024-01-25 00:04:13 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Getter @Setter
+public class Hello {
+    public String data;
+
+}
