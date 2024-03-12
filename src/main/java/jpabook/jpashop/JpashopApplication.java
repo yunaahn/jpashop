fa66c43c3dc330824a1f8478881c01abf8b02ea1
yunaahn <86057836+yunaahn@users.noreply.github.com>
2024-03-12 21:59:06 +0900

    domain

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -10,10 +10,6 @@ public class JpashopApplication {
 	public static void main(String[] args) {
 
 
-		Hello hello = new Hello();
-		hello.setData("hello");
-		String data = hello.getData();
-
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
